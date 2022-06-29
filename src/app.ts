4b726587fbc5da1478f8528258aef9ad4cbe8ab4
fabogit <fabo.dev.email@gmail.com>
2022-06-29 19:24:52 +0200

    helmet
    set headers for security

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
 import routes from "./routes/books.route";
 
 const app = express();
@@ -8,47 +9,13 @@ app.use(express.json());
 // parse url encoded req.body
 // app.use(express.urlencoded({ extended: true }));
 
-routes(app);
+// check res.headers
+app.use(helmet());
 
-// Error handling
-async function throwsError() {
-	throw new Error('Woops!');
-}
+app.get('/helmet', (req: Request, res: Response) => {
 
-app.get('/error', async (req: Request, res: Response) => {
-	try {
-		await throwsError();
-		res.sendStatus(200);
-	} catch (error) {
-		res.status(400).send('Error!');
-	}
-});
+	res.sendStatus(200);
 
-// middleware
-function handleallMethodsOne(req: Request, res: Response, next: NextFunction) {
-	res.send(req.params.foo);
-	next();
-}
-function handleallMethodsTwo(req: Request, res: Response, next: NextFunction) {
-	return res.send(req.params.bar);
-}
-
-app.all('/api/all/:foo/:bar', handleallMethodsOne, handleallMethodsTwo);
-
-// Function currying
-const middleware = ({ name }: { name: string; }) => (req: Request, res: Response, next: NextFunction) => {
-	res.locals.name = name;
-	console.log(`Middleware`);
-	next();
-};
-
-app.use(middleware({ name: 'Some name' }));
-// add typing for name to the Request generics, params is 1st req.body 3d
-app.get('/typing/:paramName', (req: Request<{ paramName: string; }, {}, { name: string; }, {}>, res: Response, next: NextFunction) => {
-	return res.send({
-		params: req.params.paramName,
-		body: req.body.name
-	});
 });
 
 app.listen(3000, () => {
